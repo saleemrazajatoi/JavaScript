b771e4f26cd1a2164e49045dd338cf3e202745b0
saleemrazajatoi <135902705+saleemrazajatoi@users.noreply.github.com>
2023-08-07 16:26:19 +0500

    Initial commit

---
+++ javascript 1st Assigment/alert.js
@@ -0,0 +1,95 @@
+alert("Error! please enter a valid password.")
+
+alert("welcome to JS land...\nHappy Coding!")
+alert("Welcome to JS Land...")
+alert("prevent this page from creating additional dialogs.")
+alert("Hello...I can run JS through my web browser's consol")
+
+
+var fullName="Saleem"
+
+alert(fullName)
+
+
+var age="15 years"
+
+alert(age)
+
+var course="Certified Mobile Application Development"
+
+alert(course)
+
+
+var food="PIZZA\nPIZZ\nPIZ\nPI\nP"
+
+alert(food)
+
+var Email="razisaleemjatoi82@gmail.com"
+
+alert(Email)
+
+var Message="I am trying to learn form the Book a smarter way to learn javaScript "
+
+alert(Message)
+
+
+var design="▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+
+alert(design)
+
+
+var myage=" I am in 15 years old"
+
+alert(myage)
+
+var visit=" you have visited this site 14 times"
+
+alert(visit)
+
+var text1="Variable names can only contain, Number,$and _ .For example : $my _1stVariable <br> Variables must begin with a letters,$ or_. For example $name, _name or name <br> Variable names are case sensitive <br>Variable names should not be JS keyboards"
+
+document.write(text1)
+
+
+var num1=3
+var num2=5
+
+document.write("<br><br> <br> <br> <b>Sum of 3 and 5 is 8</b>")
+
+
+
+document.write("<br> <br> <br> <br>  Value after vareaible declaration is undifined")
+document.write("<br>Initail value:5")
+document.write("<br>Value  after increment is :6")
+document.write("<br>Value  after addition  is :13")
+document.write("<br>Value  after decrement  is :12")
+document.write("<br> The reminder  is :0")
+
+
+document.write("<br><br> <br> <b>Total cost to buy 5 tickets to a movie is3000pkr</b>")
+
+
+document.write("<br><br> <br> <b>Table of 4</b>")
+
+
+document.write("<br><b> 4x1=4 <br> 4x2=8 <br>4x3=12 <br> 4x4=16 <br> 4x5=20 <br> 4x6=24 <br>4x7=28 <br> 4x8=32 <br> 4x9=36 <br>4x10=40 <br></b>")
+
+
+
+document.write("<br><br><br>25<sup>0</sup>C is 77<sup>o</sup>F <br>70<sup>0</sup>F is 21.11111111111111<sup>o</sup>C")
+
+
+document.write("<br><br> <h1>Shopping Cart</h1><br> <br>Prise of item  1 is 650<br>Quaintity of items 1 is 3<br>Prise of item  100 is 200<br>Quaintity of items 2 is 7<br>Shipping Charges 100 <br><br>Total cost of your order is 2750")
+
+
+document.write("<br><br> <h1>Marks Sheet</h1><br> <br>Total marks :980<br>Marks obtained :804<br>Perdentage:82.0408163265306%")
+
+
+document.write("<br><br> <h1>Currency in PKR</h1><br> <br>Total Currency in PKR:1748")
+
+document.write("<br><br> <h1>Age Calculator</h1><br> <br>Curnt Year : 2016<br>Birth Year:1992<br>Your Age is:24")
+
+document.write("<br><br> <h1>The Geometrizer</h1><br> <br>Radius of a Circle : 20<br>The circumbference is :125.999999999999<br>The Area is:1256.8")
+
+
+document.write("<br><br> <h1>The Lifetime Supply Calculator</h1><br> <br>Favouri snack:chocolate chip<br>Current age :15<br>Estimated MAximum Age:65<br>Amount of snacks per day:3<br>You will need 150 chocolate chip to last you until the ripeold age of 65")
