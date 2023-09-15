c42bb49462dd3e575c88a2a0a8b9a2792331e4ca
saleemrazajatoi <135902705+saleemrazajatoi@users.noreply.github.com>
2023-09-15 18:02:48 +0500

    Add files via upload

---
+++ javascriot 6th Assigment/functions.js
@@ -0,0 +1,167 @@
+// ------------------------------Chapter 35 to 38----------------------------------------
+
+// task no 1
+
+// function currentDate(){
+
+//     var todayDate= new Date();
+//     document.write(todayDate);
+// }
+
+// currentDate();
+
+
+// task no 2
+
+// function Struggle(){
+
+// var a =prompt("Enter your first name");
+// var b =prompt("Enter your last name");
+// var c =a + " "+b;
+// document.write("Sathi" + " " + c + " " +"Lal Slam" );
+
+// }
+
+// Struggle();
+
+// task no 3
+
+// function add(){
+//     var a=+prompt("enter number 1");
+//     var b=+prompt("enter number 2");
+//     var c= a+b;
+//     document.write(c)
+// }add();
+
+
+// task no 4
+
+// function Calc(){
+//     var a=+prompt("enter number 1");
+//     var b=+prompt("enter number 2");
+//     var sign=prompt("enter sign like +,-,/,*,%");
+//     var sum=a+b;
+//     var sub=a-b;
+//     var mul=a*b;
+//     var div=a/b;
+//     var rem=a%b;
+//     if(sign==="+"){
+//         document.write(sum);
+//     }
+//     else if(sign==="-"){
+//         document.write(sub);
+//     }
+//     else if(sign==="*"){
+//         document.write(mul);
+//     }
+//     else if(sign==="/"){
+//         document.write(div);
+//     }
+//     else if(sign==="%"){
+//         document.write(rem);
+//     }
+//     else {
+//         document.write("error");
+//     }
+//     }
+//     Calc();
+
+    // task no 5
+
+    // function square(){
+    //     var a=+prompt("enter num for spuare");
+    //     var b=a*a;
+    //     document.write(b);
+    // }
+    // square();
+
+    // task no 6
+
+//     function factorial(n){
+//     var answer = 1;
+//     if (n == 0 || n == 1){
+//       return answer;
+//     }else{
+//       for(var i = n; i >= 1; i--){
+//         answer = answer * i;
+//       }
+//       return answer;
+//     }  
+//   }
+//   var n = +prompt("enter num for factorial");
+//   answer = factorial(n)
+//   document.write("The factorial of " + n + " is " + answer);
+
+
+// task no 7
+
+
+// function counting(){
+//     var a=+prompt("starting num")
+//     var b=+prompt("ending num")
+//     for(a;a<=sb;a++){
+//        document.write(a+"<br>");
+//     }
+// }
+// counting();
+
+
+
+// task no 8
+
+// function calcHypotenus(base,per) {
+//     var hypo= base*base  + per*per ;
+//     alert(hypo+"\n")
+//     function calcSquare(){
+//     var squrt= Math.sqrt(base*base  + per*per) ;
+//     alert(squrt)}
+//     calcSquare()
+// }
+// var base=+prompt("enter base value")
+// var per=+prompt("enter perpendicular value")
+// calcHypotenus(base,per);
+
+
+// task no 9
+
+// function area(width,height){
+//     var area=width*height;
+//     document.write(area);
+// }
+// area(10,20);
+
+// task no 10
+
+// function palindrome(){
+//     var a=prompt("enter word");
+//     var b=a.split("");
+//     var c=b.reverse().join("");
+// if(a===c){
+//     document.write(c +" " +" is palindrome word")
+// }
+// else{
+//     document.write("NOT palindrome word")
+// }
+// }
+// palindrome();
+
+
+
+
+// task no 11
+
+
+
+// task no 12
+
+
+
+// task no 13
+
+
+
+// task no 15
+
+
+
+    
